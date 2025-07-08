ffc72ddf3858dda66bf42138424c5d297be5f901
Matheus Azevedo <matheus_ma02@hotmail.com>
2025-07-08 20:52:25 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,13 @@ public class Program {
 			System.out.println(obj);
 			
 		}
+		
+		System.out.println("==== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+			
+		}
 				
 				
 				
